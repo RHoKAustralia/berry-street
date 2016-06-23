6449780979ea62b137794bb69630f95f92b83aa1
Shaun Wilde <shaun.wilde@myob.com>
2016-06-23 19:36:11 +1000

    decode the token

--- spikes/api/server.js
+++ spikes/api/server.js
@@ -1,11 +1,18 @@
 var express = require('express')
   , cors = require('cors')
-  , app = express();
+  , app = express()
+  , jwt = require('jsonwebtoken');
 
+var client_id = 'MxPklQCyko4T1lfQmZOAQnSA7ZhCYDN7';
+  
 app.use(cors());
 
 app.get('/products/:id', function(req, res, next){
-  console.log(JSON.stringify(req.headers));
+  //console.log(JSON.stringify(req.headers));
+  var token = req.headers['authorization'];
+  console.log(token);
+  var decoded = jwt.decode(token.replace('Bearer ', ''));
+  console.log(decoded);
   res.json({msg: 'This is CORS-enabled for all origins!'});
 });
 
