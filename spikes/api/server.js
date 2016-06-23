14ca0ca2bd0ef6faf077050fbd460aa9f10016b6
Shaun Wilde <shaun.wilde@myob.com>
2016-06-23 20:34:14 +1000

    use express-jwt to verify the token

--- spikes/api/server.js
+++ spikes/api/server.js
@@ -1,18 +1,12 @@
 var express = require('express')
   , cors = require('cors')
   , app = express()
-  , jwt = require('jsonwebtoken');
-
-var client_id = 'MxPklQCyko4T1lfQmZOAQnSA7ZhCYDN7';
+  , secret = require('./secret.js');
   
 app.use(cors());
+app.use(secret.jwtCheck);
 
 app.get('/products/:id', function(req, res, next){
-  //console.log(JSON.stringify(req.headers));
-  var token = req.headers['authorization'];
-  console.log(token);
-  var decoded = jwt.decode(token.replace('Bearer ', ''));
-  console.log(decoded);
   res.json({msg: 'This is CORS-enabled for all origins!'});
 });
 
