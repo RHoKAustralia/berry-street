ed74ae7a12c2c8ab212f8cb87a0d632c1e9f7fa6
Edwin Granados <edwingranados@yahoo.com>
2016-08-15 08:57:08 +1000

    fix handling of graphId

--- api/src/main/groovy/au/org/berrystreet/familyfinder/api/domain/Entity.groovy
+++ api/src/main/groovy/au/org/berrystreet/familyfinder/api/domain/Entity.groovy
@@ -1,11 +1,12 @@
 package au.org.berrystreet.familyfinder.api.domain
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.voodoodyne.jackson.jsog.JSOGGenerator
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import io.swagger.annotations.ApiModel
+import org.neo4j.ogm.annotation.GraphId
 
+@ApiModel
 @JsonIdentityInfo(generator=JSOGGenerator)
-@JsonIgnoreProperties(value=['@id', 'MetaClass'],ignoreUnknown=true)
-public abstract class Entity {
-    Long id
+class Entity {
+    @GraphId Long id
 }
