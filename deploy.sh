15c09e6dd01bd9370bdd37f76ba409a490243ff4
Ubuntu <ubuntu@ip-172-31-36-208.ap-southeast-2.compute.internal>
2016-11-26 05:02:24 +0000

    Run from cron

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,35 @@
 # Will start frontend on port 80, API on 8080
 # and neo4j on 7474
 
+# This can be run from cron. If it detects changes on the prod branch
+# then it will merge and run them
+#
+# To deploy code from github, open a pull request from master to prod
+# Merge the pull and wait up to 5 mins
+
 set -e
 
+BRANCH=prod
+if [ "$1" = "--force" ] ; then
+	DEPLOY=true
+else
+	DEPLOY=false
+fi
+
 cd ~/berry-street
-git pull
-docker-compose build
-docker-compose down
-docker-compose up -d
+git fetch origin $BRANCH
+if [ "$(git rev-parse HEAD)" = "$(git rev-parse FETCH_HEAD)" ] ; then
+	echo "No changes detected"
+else
+	echo "Deploying changes"
+	git log --reverse --no-merges --stat HEAD..FETCH_HEAD
+	git merge FETCH_HEAD
+	DEPLOY=true
+fi
+
+if [ "$DEPLOY" = "true" ] ; then
+	docker-compose build
+	docker-compose down --remove-orphans
+	docker-compose up -d
+fi
 
