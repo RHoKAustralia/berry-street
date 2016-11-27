0ff5734d85db0b5227379adf9cf8304a891ee2cc
jumpinjackie <jumpinjackie@gmail.com>
2016-11-27 11:35:50 +1100

    - Switch back to the real backend - Fix bad response checking. Response.ok is a property not a function!

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,19 +1,305 @@
-const CASE_SUMMARY_DATA = [
-  { surname: "Simpson", staffName: "", firstname: "Bart", phase: "New", caseNumber: "1" },
-  { surname: "Simpson", staffName: "", firstname: "Lisa", phase: "Talking", caseNumber: "2" },
-  { surname: "Van Houten", staffName: "", firstname: "Milhouse", phase: "New", caseNumber: "3" },
-  { surname: "Wiggum", staffName: "", firstname: "Ralph", phase: "New", caseNumber: "4" },
-  { surname: "Muntz", staffName: "", firstname: "Nelson", phase: "Closed", caseNumber: "5" },
-  { surname: "Simpson", staffName: "", firstname: "Maggie", phase: "Closed", caseNumber: "6" }
+const CASE_DATA = [
+  {
+        "@id": "1",
+        "id": 191,
+        "familyFinderStaffName": "Dolores",
+        "caseManager": "Jen",
+        "status": "Open",
+        "dateOpened": "2016-08-05",
+        "dateClosed": null,
+        "caseObjective": "test Neo4J",
+        "phaseOfInvolvement": "Referred",
+        "subjects": [
+            {
+                "@id": "2",
+                "id": 255,
+                "person": {
+                    "@id": "3",
+                    "id": 182,
+                    "name": "Bart Simpson",
+                    "dateOfBirth": "1987-04-19",
+                    "family": [],
+                    "friends": []
+                },
+                "aCase": {
+                    "@ref": "1"
+                },
+                "date": "2016-08-05"
+            }
+        ]
+    }
+];
+const PEOPLE_DATA = [
+  {
+    "@id": "1",
+    "id": 180,
+    "name": "Maggie Simpson",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": []
+  },
+  {
+    "@id": "2",
+    "id": 189,
+    "name": "Patty Bouvier",
+    "dateOfBirth": "1987-04-19",
+    "family": [
+      {
+        "@id": "3",
+        "id": 248,
+        "kin": {
+          "@ref": "2"
+        },
+        "person": {
+          "@id": "4",
+          "id": 181,
+          "name": "Lisa Simpson",
+          "dateOfBirth": "1987-04-19",
+          "family": [],
+          "friends": []
+        },
+        "relationship": "aunt"
+      },
+      {
+        "@id": "5",
+        "id": 247,
+        "kin": {
+          "@ref": "2"
+        },
+        "person": {
+          "@ref": "1"
+        },
+        "relationship": "aunt"
+      },
+      {
+        "@id": "6",
+        "id": 249,
+        "kin": {
+          "@ref": "2"
+        },
+        "person": {
+          "@id": "7",
+          "id": 182,
+          "name": "Bart Simpson",
+          "dateOfBirth": "1987-04-19",
+          "family": [],
+          "friends": []
+        },
+        "relationship": "aunt"
+      }
+    ],
+    "friends": []
+  },
+  {
+    "@id": "8",
+    "id": 183,
+    "name": "Marge Simpson",
+    "dateOfBirth": "1987-04-19",
+    "family": [
+      {
+        "@id": "9",
+        "id": 244,
+        "kin": {
+          "@ref": "8"
+        },
+        "person": {
+          "@ref": "4"
+        },
+        "relationship": "parent"
+      },
+      {
+        "@id": "10",
+        "id": 243,
+        "kin": {
+          "@ref": "8"
+        },
+        "person": {
+          "@ref": "1"
+        },
+        "relationship": "parent"
+      },
+      {
+        "@id": "11",
+        "id": 245,
+        "kin": {
+          "@ref": "8"
+        },
+        "person": {
+          "@ref": "7"
+        },
+        "relationship": "parent"
+      }
+    ],
+    "friends": []
+  },
+  {
+    "@id": "12",
+    "id": 184,
+    "name": "Homer Simpson",
+    "dateOfBirth": "1987-04-19",
+    "family": [
+      {
+        "@id": "13",
+        "id": 241,
+        "kin": {
+          "@ref": "12"
+        },
+        "person": {
+          "@ref": "4"
+        },
+        "relationship": "parent"
+      },
+      {
+        "@id": "14",
+        "id": 240,
+        "kin": {
+          "@ref": "12"
+        },
+        "person": {
+          "@ref": "1"
+        },
+        "relationship": "parent"
+      },
+      {
+        "@id": "15",
+        "id": 242,
+        "kin": {
+          "@ref": "12"
+        },
+        "person": {
+          "@ref": "7"
+        },
+        "relationship": "parent"
+      },
+      {
+        "@id": "16",
+        "id": 246,
+        "kin": {
+          "@ref": "12"
+        },
+        "person": {
+          "@ref": "8"
+        },
+        "relationship": "partner"
+      }
+    ],
+    "friends": []
+  },
+  {
+    "@ref": "4"
+  },
+  {
+    "@id": "17",
+    "id": 186,
+    "name": "Ralph Wiggum",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": [
+      {
+        "@id": "18",
+        "id": 251,
+        "kith": {
+          "@ref": "17"
+        },
+        "person": {
+          "@ref": "4"
+        },
+        "relationship": "friend"
+      }
+    ]
+  },
+  {
+    "@ref": "7"
+  },
+  {
+    "@id": "19",
+    "id": 187,
+    "name": "Groundskeeper Willie",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": [
+      {
+        "@id": "20",
+        "id": 252,
+        "kith": {
+          "@ref": "19"
+        },
+        "person": {
+          "@ref": "7"
+        },
+        "relationship": "grounskeeper"
+      }
+    ]
+  },
+  {
+    "@id": "21",
+    "id": 188,
+    "name": "Nelson Muntz",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": [
+      {
+        "@id": "22",
+        "id": 253,
+        "kith": {
+          "@ref": "21"
+        },
+        "person": {
+          "@ref": "7"
+        },
+        "relationship": "friend"
+      }
+    ]
+  },
+  {
+    "@id": "23",
+    "id": 185,
+    "name": "Milhouse Van Houten",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": [
+      {
+        "@id": "24",
+        "id": 250,
+        "kith": {
+          "@ref": "23"
+        },
+        "person": {
+          "@ref": "7"
+        },
+        "relationship": "friend"
+      }
+    ]
+  },
+  {
+    "@id": "25",
+    "id": 190,
+    "name": "Ned Flanders",
+    "dateOfBirth": "1987-04-19",
+    "family": [],
+    "friends": [
+      {
+        "@id": "26",
+        "id": 254,
+        "kith": {
+          "@ref": "25"
+        },
+        "person": {
+          "@ref": "12"
+        },
+        "relationship": "neighbour"
+      }
+    ]
+  }
 ];
 
 const CASE_DETAILS_DATA = [
-  { caseNumber: "1" },
-  { caseNumber: "2" },
-  { caseNumber: "3" },
-  { caseNumber: "4" },
-  { caseNumber: "5" },
-  { caseNumber: "6" }
+  { id: 1, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] },
+  { id: 2, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] },
+  { id: 3, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] },
+  { id: 4, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] },
+  { id: 5, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] },
+  { id: 6, "familyFinderStaffName": "Dolores", "caseManager": "Jen", "status": "Open", "dateOpened": "2016-08-05", "dateClosed": null, "caseObjective": "test Neo4J", "phaseOfInvolvement": "Referred", subjects: [] }
 ];
 
 const cases = [
@@ -22,7 +308,7 @@ const cases = [
   { caseId: "12347", staffName: "Jenny", childName: "Kate Smith", caseNumber: "234958" },
 ];
 
-export const MOCK_BACKEND = true;
+export const MOCK_BACKEND = false;
 export const SERVICE_URL_BASE = "http://localhost:8080";
 
 function sendRequest(url, method, data) {
@@ -37,7 +323,7 @@ function sendRequest(url, method, data) {
   }
   const request = new Request(url, requestOpts)
   return fetch(request).then(r => {
-    if (r.ok()) {
+    if (r.ok) {
       return r.json();
     }
   });
@@ -45,10 +331,10 @@ function sendRequest(url, method, data) {
 
 export default {
   getCaseStatuses() {
-    return Promise.resolve([ "Open", "Closed" ])
+    return Promise.resolve(["Open", "Closed"])
   },
   getPhases() {
-    return Promise.resolve([ "Referred", "Searching" ])
+    return Promise.resolve(["Referred", "Searching"])
   },
   getUsers() {
     return new Promise((resolve, reject) => {
