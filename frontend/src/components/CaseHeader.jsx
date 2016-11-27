9c4bafdab656762a63554972c1246998104f1e27
jumpinjackie <jumpinjackie@gmail.com>
2016-11-27 13:14:55 +1100

    Use actual perosn image in case header

--- frontend/src/components/CaseHeader.jsx
+++ frontend/src/components/CaseHeader.jsx
@@ -7,13 +7,14 @@ const TILE_HEIGHT = 100;
 class CaseHeader extends Component {
   render() {
     if (!this.props.case) return (<p>Case prop missing...</p>)
+    const subject = this.props.case.subjects[0];
     return <div className="row page-header">
       <div className="col-xs-12 col-sm-6 col-md-9">
-        <h1>{this.props.case.subjects[0].person.name}</h1>
+        <h1>{subject.person.name}</h1>
         <p style={{'fontSize': '1.4em'}}><span style={{'color': 'rgba(199,196,196,1)'}}>Phase of Involvement:</span> {this.props.case.phaseOfInvolvement}</p>
       </div>
       <div className="col-xs-12 col-sm-6 col-md-3">
-        <img src={`http://lorempixel.com/${TILE_WIDTH}/${TILE_HEIGHT}/cats/1`} className="img-responsive pull-right" />
+        <img src={subject.person.image || `src/assets/images/child_case_720.jpg`} className="img-responsive pull-right" />
       </div>
     </div>
   }
