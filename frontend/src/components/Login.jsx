7dd9f1bf6b5571c00d5c9010dc66541cdf375c13
Yael Di Censo <yael.garnham@gmail.com>
2016-11-27 14:20:54 +1100

    styling login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,10 +12,17 @@ export default React.createClass({
 
   render: function () {
     return (
+	<div className="container">
+	<div className="row">
       <div className="login-box auth0-box before">
-        <img src="src/assets/images/logo.png" className="center-block" />
+	<div className="col-md-12">        
+	<img src="src/assets/images/logo.png" className="center-block" />
+	
         <div>&nbsp; </div>
         <a onClick={this.showLock} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+	</div>
+	</div>
+	</div>
       </div>);
   }
 });
