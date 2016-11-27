ddf837584e274036448b91ff3b094efc55cc4dd2
Kelvin Lau <kelvin22@gmail.com>
2016-11-27 13:05:09 +1100

    Add icon for risk traffic light

--- frontend/src/components/PersonRelationship.jsx
+++ frontend/src/components/PersonRelationship.jsx
@@ -5,7 +5,8 @@ const PersonRelationship = React.createClass({
   render() {
     return (
         <div className="PersonRelationship">
-            <div className="PersonRelationship-riskStatus">{this.props.riskStatus}</div>
+
+        	<div className="PersonRelationship-riskStatus"><span className={ "glyphicon glyphicon-stop " + this.props.riskStatus } aria-hidden="true"></span></div>
             <div className="PersonRelationship-personName">{this.props.personName}</div>
             <div className="PersonRelationship-relationship">{this.props.relationship}</div>
         </div>
