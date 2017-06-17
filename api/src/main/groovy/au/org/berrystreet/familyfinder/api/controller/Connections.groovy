09c00aa1fd957f75b14b527a159edd9de579dcd4
JohnnyNiu <johnny.x.niu@gmail.com>
2017-06-17 19:57:22 +1000

    Remove connections of person concept. Added endpoint connections of case

--- api/src/main/groovy/au/org/berrystreet/familyfinder/api/controller/Connections.groovy
+++ api/src/main/groovy/au/org/berrystreet/familyfinder/api/controller/Connections.groovy
@@ -19,32 +19,20 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET
 import static org.springframework.web.bind.annotation.RequestMethod.PUT
 
 @RestController
-@RequestMapping(value = '/connections', produces = [APPLICATION_JSON])
-@Api(value = '/connections', description = 'the connections API')
+@RequestMapping(value = '/cases/{caseId}/connections', produces = [APPLICATION_JSON])
+@Api(value = '/cases/{caseId}/connections', description = 'the connections API')
 @CrossOrigin(origins = '*')
 class Connections {
 
     @Autowired
     ConnectionService connectionService
 
-    @ApiOperation(value = '', notes = 'Gets connections of `Person` identified with `id`', response = Connection)
+    @ApiOperation(value = '', notes = 'Gets connections of `Case` identified with `id`', response = Connection)
     @ApiResponses(value = [@ApiResponse(code = 200, message = 'Successful response', response = Connection)])
-    @RequestMapping(
-            value = '/{id}/connections',
-            method = GET)
+    @RequestMapping(method = GET)
     List<Connection> listConnections(
-            @ApiParam(value = 'ID of person to fetch', required = true) @PathVariable('id') Long id) {
-        connectionService.getConnections(id)
+            @ApiParam(value = 'ID of case to fetch', required = true) @PathVariable('caseId') Long caseId) {
+        connectionService.getConnections(caseId)
     }
 
-    @RequestMapping(
-            value = '/{fromId}/connections',
-            method = PUT)
-    Iterable<Connection> saveConnection(
-            @ApiParam(value = 'this person', required = true) @PathVariable('fromId') Long fromId,
-            @ApiParam(value = 'to', required = true) @RequestParam('toId') Long toId,
-            @ApiParam(value = 'relationship', required = true) @RequestParam('relationship') String relationship,
-            @ApiParam(value = 'notes', required = false) @RequestParam('notes') String notes) {
-        connectionService.create(toId, fromId, relationship, notes)
-    }
 }
