455f5e8cb40771b0350f33c07edd7f4eb6e94860
Andras Bubics <abubics@dius.com.au>
2017-11-25 16:43:18 +1100

    Make groups and people scoped by case
    - Also move versions out of build.gradle.

--- api/src/main/groovy/au/org/berrystreet/familyfinder/api/repositories/CaseRepository.groovy
+++ api/src/main/groovy/au/org/berrystreet/familyfinder/api/repositories/CaseRepository.groovy
@@ -4,5 +4,4 @@ import au.org.berrystreet.familyfinder.api.domain.Case
 import org.springframework.data.neo4j.repository.GraphRepository
 
 interface CaseRepository extends GraphRepository<Case> {
-
 }
--- api/src/main/groovy/au/org/berrystreet/familyfinder/api/repositories/GraphNodeRepository.groovy
+++ api/src/main/groovy/au/org/berrystreet/familyfinder/api/repositories/GraphNodeRepository.groovy
@@ -4,5 +4,4 @@ import au.org.berrystreet.familyfinder.api.domain.internals.GraphNode
 import org.springframework.data.neo4j.repository.GraphRepository
 
 interface GraphNodeRepository extends GraphRepository<GraphNode> {
-
 }
